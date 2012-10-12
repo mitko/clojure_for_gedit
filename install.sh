83ee349c9079554715edcdce7eade024332657f2
logan <logan@logan-ubuntu.(none)>
2012-10-12 18:21:25 +1300

    Gtk 3.0 is commonly used.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo cp clojure.lang /usr/share/gtksourceview-2.0/language-specs/clojure.lang
+sudo cp clojure.lang /usr/share/gtksourceview-3.0/language-specs/clojure.lang
 sudo cp clojure.xml /usr/share/mime/packages/clojure.xml
 cd /usr/share/
 sudo update-mime-database mime
