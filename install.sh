b49d8cd95469b97260604514a27470e950bb0cd2
Glen K. Peterson <glen@organicdesign.org>
2013-04-18 08:57:21 -0300

    Handle gtksourceview 3.0 and prompt user
    Updated install script to handle gtksourceview 3.0 and also to prompt user for what is happening and give them a choice about continuing.

--- install.sh
+++ install.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
 
-sudo cp clojure.lang /usr/share/gtksourceview-2.0/language-specs/clojure.lang
-sudo cp clojure.xml /usr/share/mime/packages/clojure.xml
-cd /usr/share/
-sudo update-mime-database mime
-sudo apt-get install clojure
+read -p "Install Clojure syntax highlighting in Gedit? (y/n)? " answer
+case $answer in
+    N|n|No|NO|no) echo 'Exiting...'
+        ;;
+    *) echo 'Yes'
+        if [ -d "/usr/share/gtksourceview-2.0" ]
+        then
+          sudo cp clojure.lang /usr/share/gtksourceview-2.0/language-specs/clojure.lang
+        elif [ -d "/usr/share/gtksourceview-3.0" ]
+        then
+          sudo cp clojure.lang /usr/share/gtksourceview-3.0/language-specs/clojure.lang
+        else
+          echo "Couldn't find /usr/share/gtksourceview folder (only checked for version 2.0 and 3.0)"
+        fi
 
+        sudo cp clojure.xml /usr/share/mime/packages/clojure.xml
+        cd /usr/share/
+        sudo update-mime-database mime
+
+        read -p "Install/update clojure package on your OS (apt-get)? (y/n)? " answer
+        case $answer in
+            y|Y|Yes|yes|YES)
+                echo 'Yes' 
+                sudo apt-get install clojure
+                ;;
+            *) echo 'Exiting...'
+                ;;
+        esac
+        ;;
+esac
 
 
